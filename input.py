5fabc01effb5baf30302daddbceafd422bff4523
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-21 17:48:50 +0700

    coba update

--- input.py
+++ input.py
@@ -7,4 +7,6 @@ data = pd.read_csv('V000147.TXT', sep='\t', header=21) #Data reading, header cut
 data = data.iloc[1:-4, 0:2] #null data @index 0 removed, remove data at the end of aquisition #!(4 last data)
 data.Timestamp = pd.to_datetime(data.Timestamp, dayfirst=True)
 data.index = data.Timestamp
+data = data.iloc[:,1]
+
 print(data)
