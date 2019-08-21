b050b078fc8268d8dae553c2c65a9e6d186c6a2f
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-21 19:04:41 +0700

    add output

--- input.py
+++ input.py
@@ -3,10 +3,11 @@ import pandas as pd
 from scipy import fftpack
 from matplotlib import pyplot as plt 
 
-data = pd.read_csv('V000147.TXT', sep='\t', header=21) #Data reading, header cutting, line number 21 = column
-data = data.iloc[1:-4, 0:2] #null data @index 0 removed, remove data at the end of aquisition #!(4 last data)
-data.Timestamp = pd.to_datetime(data.Timestamp, dayfirst=True)
-data.index = data.Timestamp
-data = data.iloc[:,1]
+data = pd.read_csv('V000147.TXT', sep='\t', header=21) # Data reading, header cutting, line number 21 = column
+data = data.iloc[1:, 0:2] #null data @index 0 removed
+data.index = pd.to_datetime(data.Timestamp, dayfirst=True)
 
 print(data)
+
+# output
+data.to_csv('V000147_OUT.TXT', sep='\t')
