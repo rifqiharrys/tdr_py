a0b16131732c00a47744730d5c79812720bac6d0
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-21 15:52:08 +0700

    Add files via upload

---
+++ input.py
@@ -0,0 +1,10 @@
+import numpy as np 
+import pandas as pd 
+from scipy import fftpack
+from matplotlib import pyplot as plt 
+
+data = pd.read_csv('20190314/V000147.TXT', sep='\t', header=21) #Data reading, header cutting, line number 21 = column
+data = data.iloc[1:-4, 0:2] #null data @index 0 removed, remove data at the end of aquisition #!(4 last data)
+data.Timestamp = pd.to_datetime(data.Timestamp, dayfirst=True)
+data.index = data.Timestamp
+print(data)
