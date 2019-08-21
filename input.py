c8ad193f7a93e1a16d083b229506f54b1138ada9
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-21 16:36:44 +0700

    penyesuaian

--- input.py
+++ input.py
@@ -3,7 +3,7 @@ import pandas as pd
 from scipy import fftpack
 from matplotlib import pyplot as plt 
 
-data = pd.read_csv('20190314/V000147.TXT', sep='\t', header=21) #Data reading, header cutting, line number 21 = column
+data = pd.read_csv('V000147.TXT', sep='\t', header=21) #Data reading, header cutting, line number 21 = column
 data = data.iloc[1:-4, 0:2] #null data @index 0 removed, remove data at the end of aquisition #!(4 last data)
 data.Timestamp = pd.to_datetime(data.Timestamp, dayfirst=True)
 data.index = data.Timestamp
