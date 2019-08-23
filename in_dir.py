97c97a5d08b508467c73e528f71864e159010abe
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-23 11:25:37 +0700

    add in_dir

--- in_dir.py
+++ in_dir.py
@@ -1,4 +1,11 @@
 import os
+import glob
+
+dirlist = glob.glob('*/')
+for dir in dirlist:
+	os.chdir(dir)
+
+	os.chdir('..')
 
 # os.chdir() #* insert path with single quotes
 # os.listdir() #* directories & files in a list []
