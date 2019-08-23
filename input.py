b31b501ae0d2d1ffe9739243e7b1a481cf8f5f2a
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-23 16:33:28 +0700

    new tide files

--- input.py
+++ input.py
@@ -5,14 +5,17 @@ from matplotlib import pyplot as plt
 
 def valeport_data(raw):
 	data = pd.read_csv(raw, sep='\t', header=21) # Data reading, header cutting, line number 21 = column
-	data = data.iloc[1:, 0:2] #null data @index 0 removed
+	data = data.iloc[1:, 0:3] #null data @index 0 removed
 	return data
 
 txtlist = glob.glob('*.[Tt][Xx][Tt]')
 
+for txt in txtlist:
+	data_in = valeport_data(txt)
+
 #TODO: change Timestamp object type into datetime
 # data.index = pd.to_datetime(data.Timestamp, dayfirst=True)
-print(data)
+# print(data)
 
 # output
-data.to_csv('V000147_OUT.TXT', sep='\t')
+# data.to_csv('V000147_OUT.TXT', sep='\t')
