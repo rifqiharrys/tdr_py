a2d71b83c27152906bcf54c277da3562e4207fcb
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-23 14:44:52 +0700

    add function to read valeport data and make it into data frame

--- input.py
+++ input.py
@@ -2,10 +2,13 @@ import numpy as np
 import pandas as pd 
 from matplotlib import pyplot as plt 
 
-data = pd.read_csv('V000147.TXT', sep='\t', header=21) # Data reading, header cutting, line number 21 = column
-data = data.iloc[1:, 0:2] #null data @index 0 removed
-data.index = pd.to_datetime(data.Timestamp, dayfirst=True)
+def valeport_data(raw):
+	data = pd.read_csv(raw, sep='\t', header=21) # Data reading, header cutting, line number 21 = column
+	data = data.iloc[1:, 0:2] #null data @index 0 removed
+	return data
 
+#TODO: change Timestamp object type into datetime    
+# data.index = pd.to_datetime(data.Timestamp, dayfirst=True)
 print(data)
 
 # output
