5d8eac6008978aa45e02361c92c3920c010d468b
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-23 15:18:17 +0700

    add glob to list txt files

--- input.py
+++ input.py
@@ -1,11 +1,14 @@
 import numpy as np 
 import pandas as pd 
+import glob
 from matplotlib import pyplot as plt 
 
 def valeport_data(raw):
 	data = pd.read_csv(raw, sep='\t', header=21) # Data reading, header cutting, line number 21 = column
 	data = data.iloc[1:, 0:2] #null data @index 0 removed
 	return data
+
+txtlist = glob.glob('*.[Tt][Xx][Tt]')
 
 #TODO: change Timestamp object type into datetime
 # data.index = pd.to_datetime(data.Timestamp, dayfirst=True)
