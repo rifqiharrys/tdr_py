12c853f611259a31476bf608fa9052978c0745fe
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-25 14:58:26 +0700

    del unusable data & finished merge data

--- input.py
+++ input.py
@@ -6,17 +6,21 @@ import glob
 from matplotlib import pyplot as plt 
 
 def valeport_data(raw):
-	data = pd.read_csv(raw, sep='\t', header=21) # Data reading, header cutting, line number 21 = column
+	data = pd.read_table(raw, header=21, index_col='Timestamp') # Data reading, header cutting, line number 21 = column
 	data = data.iloc[1:, 0:2] #null data @index 0 removed
-	data.index = pd.to_datetime(data.Timestamp, dayfirst=True)
+	data.index = pd.to_datetime(data.index, dayfirst=True)
 	return data
 
 txtlist = glob.glob('*.[Tt][Xx][Tt]')
+dummy = []
 
 for txt in txtlist:
-	data_in = valeport_data(txt)
+	txt = valeport_data(txt)
+	dummy.append(txt)
 
-#TODO: change Timestamp object type into datetime
+data_merged = pd.concat(dummy)
+
+#TODO: sort data by Timestamp
 # data.index = pd.to_datetime(data.Timestamp, dayfirst=True)
 # print(data)
 
