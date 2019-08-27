fb09b567414d256682a2c88c9e0f33692fe1ba2c
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-27 14:14:12 +0700

    change function name

--- valeport_processing.py
+++ valeport_processing.py
@@ -3,24 +3,24 @@
 import pandas as pd 
 import glob 
 
-def valeport_input(raw):
+def v_input(raw):
 	data = pd.read_csv(raw, sep='\t', header=21, index_col='Timestamp') # Data reading, header cutting, line number 21 = column
 	data = data.iloc[1:, 0:2] #null data @index 0 removed
 	data.index = pd.to_datetime(data.index, dayfirst=True)
 	return data
 
-def valeport_merge():
+def v_merge():
 	txtlist = glob.glob('*.[Tt][Xx][Tt]')
 	dummy = []
 
 	for txt in txtlist:
-		df = valeport_input(txt)
+		df = v_input(txt)
 		dummy.append(df)
 
 	return pd.concat(dummy)
 
-def valeport_sort():
-	merged = valeport_merge()
+def v_sort():
+	merged = v_merge()
 	return merged.sort_index()
 
 
