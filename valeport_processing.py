d60b192706a4e669c086ab08d6f0d8e4c74c8477
rifqiharrys <rifqi.m.h@gmail.com>
2019-08-28 07:59:48 +0700

    no ntz

--- valeport_processing.py
+++ valeport_processing.py
@@ -3,26 +3,25 @@
 import pandas as pd 
 import glob 
 
-def v_input(raw, tz = 'UTC', ntz = 'UTC'):
+def v_input(raw, tz = 'UTC'):
 	data = pd.read_csv(raw, sep='\t', header=21, index_col='Timestamp') # Data reading, header cutting, line number 21 = column
 	data = data.iloc[1:, 0:2] #null data @index 0 removed
 	data.index = pd.to_datetime(data.index, dayfirst=True)
 	data.index = data.index.tz_localize(tz)
-	data.index = data.index.tz_convert(ntz)
 	return data
 
-def v_merge(tz = 'UTC', ntz = 'UTC'):
+def v_merge(tz = 'UTC'):
 	txtlist = glob.glob('*.[Tt][Xx][Tt]')
 	dummy = []
 
 	for txt in txtlist:
-		df = v_input(txt, tz, ntz)
+		df = v_input(txt, tz)
 		dummy.append(df)
 
 	return pd.concat(dummy)
 
-def v_sort(tz = 'UTC', ntz = 'UTC'):
-	merged = v_merge(tz, ntz)
+def v_sort(tz = 'UTC'):
+	merged = v_merge(tz)
 	return merged.sort_index()
 
 
