9493da822211fa27f30884384cf0ff46d8e78d69
rifqiharrys <rifqi.m.h@gmail.com>
2020-05-17 21:29:14 +0700

    remove timezone (tz)

--- vp_tide.py
+++ vp_tide.py
@@ -10,7 +10,7 @@ import os
 #* full tz list
 #* https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
 
-def v_input(raw, tz = 'UTC'):
+def v_input(raw):
 	# Data reading, header cutting until line number 21, data under Timestamp become index
 	data = pd.read_csv(raw, sep='\t', header=21, index_col='Timestamp')
 
@@ -21,11 +21,11 @@ def v_input(raw, tz = 'UTC'):
 	data.index = pd.to_datetime(data.index, dayfirst=True)
 
 	# Add tz to dataframe, default: 'UTC'
-	data.index = data.index.tz_localize(tz)
+	# data.index = data.index.tz_localize(tz)
 
 	return data
 
-def v_merge(tz = 'UTC'):
+def v_merge():
 	# .txt file list
 	txtlist = glob.glob('*.[Tt][Xx][Tt]')
 
@@ -34,7 +34,7 @@ def v_merge(tz = 'UTC'):
 
 	# Insert data from some .txt files using v_input and append into list
 	for txt in txtlist:
-		df = v_input(txt, tz)
+		df = v_input(txt)
 		dummy.append(df)
 
 	# Merge list of data into one unsorted dataframe
@@ -43,13 +43,13 @@ def v_merge(tz = 'UTC'):
 	# Sorted dataframe by index (i.e. Timestamp) as output
 	return merged.sort_index()
 
-def v_dirmerge(tz = 'UTC'):
+def v_dirmerge():
 	dirlist = glob.glob('*/')
 	dummy = []
 
 	for dir in dirlist:
 		os.chdir(dir)
-		df = v_merge(tz)
+		df = v_merge()
 		dummy.append(df)
 		os.chdir('..')
 	
